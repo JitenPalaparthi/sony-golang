4877bcac65b95e3744a9cb177d13398293079a3f
Jiten <jpalaparthi@gmail.com>
2023-04-15 15:51:13 +0530

    add any in pointer arth

--- 25-pointerarth/main.go
+++ 25-pointerarth/main.go
@@ -6,15 +6,15 @@ import (
 )
 
 func main() {
-	arr := []int{10, 14, 18, 19, 20, 10}
+	arr := []any{10, 14.34, false, 'A', "Hello World", 10}
 	fmt.Println(arr)
-	var ptr *int = &arr[0]
+	var ptr *any = &arr[0]
 	fmt.Println(*ptr)
 
 	for i := 1; i < len(arr); i++ {
 		var uptr uintptr = uintptr(unsafe.Pointer(ptr))
-		uptr += unsafe.Sizeof(arr[0])
-		ptr = (*int)(unsafe.Pointer(uptr))
+		uptr += unsafe.Sizeof(arr[i])
+		ptr = (*any)(unsafe.Pointer(uptr))
 		fmt.Println(*ptr)
 	}
 
